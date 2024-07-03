f4472bf8ae333e4270ce1dfe1febe0a168d4b03b
Marian <mariansusanaas@gmail.com>
2024-07-02 23:42:02 -0400

    type collector finished

--- src/semantic/type_builder.py
+++ src/semantic/type_builder.py
@@ -1,4 +1,4 @@
-from .format_visitor import *
+from ..cmp.visitor import visitor
 from ..cmp.ast_for_hulk import *
 from ..cmp.semantic import *
 
@@ -13,52 +13,52 @@ class TypeBuilder:
     def visit(self, node):
         pass
     
-    # Your code here!!!
-    # ????
 
     @visitor.when(ProgramNode)
     def visit(self, node):
         for class_declaration in node.declarations:
             self.visit(class_declaration)
 
-    # @visitor.when(ClassDeclarationNode)
-    # def visit(self, node):
-    #     self.current_type = self.context.get_type(node.id)
+        return self.context, self.errors
 
-    #     if node.parent:
-    #         try:
-    #             parent_type = self.context.get_type(node.parent)
-    #             try:
-    #                 self.current_type.set_parent(parent_type)
-    #             except SemanticError as ex:
-    #                 self.errors.append(ex.text)
-    #         except SemanticError as ex:
-    #             self.errors.append(ex.text)
-
-    #     for feature in node.features:
-    #         self.visit(feature)
-
+    
     @visitor.when(FunctionNode)
     def visit(self, node):
         param_names = []
         param_types = []
-        for param_name, param_type_str in node.params:
-            param_names.append(param_name)
+        for param in node.params:
+            p_name = param.identifier
+            p_type = param.type_name
             try:
-                param_type = self.context.get_type(param_type_str)
+                param_type = self.context.get_type(p_type)
             except SemanticError as ex:
                 self.errors.append(ex.text)
                 param_type = ErrorType()
             param_types.append(param_type)
         try:
-            type = self.context.get_type(node.type)
+            type = self.context.get_type(node.type_name)
         except SemanticError as ex:
             self.errors.append(ex.text)
             type = ErrorType()
         try:
-            self.current_type.define_method(node.id,param_names,param_types,type) 
+            self.current_type.define_method(node.identifier, param_names, param_types, type) 
         except SemanticError as ex:
             self.errors.append(ex.text)
+
+
+
+    @visitor.when(TypeNode)
+    def visit(self, node):
+        try:
+            self.current_type = self.context.get_type(node.identifier)
+        except SemanticError as ex:
+            self.errors.append(ex.text)
+            self.current_type = ErrorType()
+            return
+        for attr in node.attr_list:
+            self.visit(attr)
+        self.current_type = None
+
 
     # @visitor.when(AttrDeclarationNode)
     # def visit(self,node):
--- src/semantic/type_collector.py
+++ src/semantic/type_collector.py
@@ -16,27 +16,23 @@ class TypeCollector(object):
         self.context = Context()
 
         self.context.create_type('None')
-        self.context.create_type('<void>')      
-        # self.context.types['void'] = VoidType()
-        # self.context.types['error'] = ErrorType()
-
 
         # Object Type (creation only)
-        type_obj = self.context.create_type('obj')
+        type_obj = self.context.create_type('Object')
 
         # Number Type        
-        type_num = self.context.create_type('num')
+        type_num = self.context.create_type('Number')
         type_num.set_parent(type_obj)
 
-        type_bool = self.context.create_type('bool')
+        type_bool = self.context.create_type('Boolean')
         type_bool.set_parent(type_obj)
 
         # String Type
-        type_str = self.context.create_type('str')
+        type_str = self.context.create_type('String')
         type_str.set_parent(type_obj)
         type_str.define_method('current', [], [], type_str)
         type_str.define_method('next', [], [], type_bool)
-        type_str.define_method('len', [], [], type_num)
+        type_str.define_method('size', [], [], type_num)
 
         # Object Type (methods definition)
         type_obj.define_method('equals', ['other'], [type_obj], type_bool)
@@ -52,19 +48,24 @@ class TypeCollector(object):
         f_log = self.context.get_type('Function').define_method('log', ['base', 'value'], [type_num, type_num], type_num)
         f_sin = self.context.get_type('Function').define_method('sin', ['value'], [type_num], type_num)
         f_cos = self.context.get_type('Function').define_method('cos', ['value'], [type_num], type_num)
-        f_rand = self.context.get_type('Function').define_method('rand', [], [], type_num)
-
+        f_rand = self.context.get_type('Function').define_method('rand', [], [], type_num)    
+        f_range = self.context.get_type('Function').define_method('range', ['start', 'end'], [type_num, type_num], type_obj)
 
         for declaration in node.decl_list:
             self.visit(declaration)
 
         return self.context, self.errors
         
-    # Your code here!!!
-    # wtf
-    # @visitor.when(ClassDeclarationNode)
-    # def visit(self, node):
-    #     try:
-    #         self.context.create_type(node.id)   
-    #     except SemanticError as ex:
-    #         self.errors.append(ex.text)
+    @visitor.when(TypeNode)
+    def visit(self, node: TypeNode):
+        try:
+            self.context.create_type(node.identifier)   
+        except SemanticError as ex:
+            self.errors.append(ex.text)
+
+    @visitor.when(ProtocolNode)
+    def visit(self, node: ProtocolNode):
+        try:
+            self.context.create_type(node.identifier)   
+        except SemanticError as ex:
+            self.errors.append(ex.text)
